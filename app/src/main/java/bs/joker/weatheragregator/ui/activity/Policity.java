9ec4a96ced99214747b4fa973993536549fd5652
bsjoker <bs-joker@yandex.ru>
2019-09-11 13:10:21 +0500

    Добавлена политика конфиденциальности.

---
+++ app/src/main/java/bs/joker/weatheragregator/ui/activity/Policity.java
@@ -0,0 +1,76 @@
+package bs.joker.weatheragregator.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.InvalidClassException;
+import java.util.List;
+
+import bs.joker.weatheragregator.R;
+import bs.joker.weatheragregator.model.PreferencesHelper;
+import bs.joker.weatheragregator.model.geonames.Geoname;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.internal.IOException;
+
+public class Policity extends BaseActivity {
+    public static final String TAG = "Policity";
+    TextView mTextView;
+    private List<Geoname> cities;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_policity);
+        mTextView = findViewById(R.id.tvPolicity);
+        mTextView.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public void onClickAgree(View view) {
+        Realm realm = Realm.getDefaultInstance();
+        try {
+            PreferencesHelper.savePreference("agreePolicity", true);
+        } catch (InvalidClassException e) {
+            e.printStackTrace();
+        }
+        try {
+            final RealmResults<Geoname> realmResults = realm.where(Geoname.class).findAll();
+            realm.executeTransaction(inRealm -> {
+                cities = realm.copyFromRealm(realmResults);
+                if (cities.size() > 0) {
+                    try {
+                        PreferencesHelper.savePreference("ChangeCityHourly", true);
+                        PreferencesHelper.savePreference("ChangeCityDaily", true);
+                        PreferencesHelper.savePreference("ChangeCityWeekly", true);
+                    } catch (InvalidClassException e) {
+                        e.printStackTrace();
+                    }
+                    Intent intent = new Intent(this, ScrollActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(this, SearchActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                Log.d(TAG, "Количество городов: " + cities.size());
+            });
+        } catch (IOException e) {
+            Log.d(TAG, e.getMessage());
+        }
+    }
+
+    public void onClickClose(View view) {
+        try {
+            PreferencesHelper.savePreference("agreePolicity", false);
+        } catch (InvalidClassException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+}
--- app/src/main/java/bs/joker/weatheragregator/ui/activity/SplashActivity.java
+++ app/src/main/java/bs/joker/weatheragregator/ui/activity/SplashActivity.java
@@ -5,18 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.arellomobile.mvp.MvpAppCompatActivity;
-import com.mikepenz.materialdrawer.model.interfaces.Nameable;
-
 import java.io.InvalidClassException;
 import java.util.List;
 
 import bs.joker.weatheragregator.R;
 import bs.joker.weatheragregator.model.PreferencesHelper;
-import bs.joker.weatheragregator.model.geonames.GeoNames;
 import bs.joker.weatheragregator.model.geonames.Geoname;
 import io.realm.Realm;
-import io.realm.RealmObject;
 import io.realm.RealmResults;
 import io.realm.internal.IOException;
 
@@ -25,9 +20,8 @@ import io.realm.internal.IOException;
  */
 
 public class SplashActivity extends BaseActivity {
-    public static final String TAG = "FlashActivity";
+    public static final String TAG = "SplashActivity";
     private List<Geoname> cities;
-    TextView textView;
     public static SplashActivity linkSplash;
 
     @Override
@@ -37,38 +31,43 @@ public class SplashActivity extends BaseActivity {
 
         linkSplash = this;
 
-        if (PreferencesHelper.getSharedPreferences().getBoolean("firstStart", true)){
+        if (PreferencesHelper.getSharedPreferences().getBoolean("firstStart", true) || !PreferencesHelper.getSharedPreferences().getBoolean("agreePolicity", true)){
             try {
                 PreferencesHelper.savePreference("langOld", getApplicationContext().getResources().getConfiguration().locale.getLanguage());
                 PreferencesHelper.savePreference("firstStart", false);
+                Intent intent = new Intent(this, Policity.class);
+                startActivity(intent);
+                finish();
             } catch (InvalidClassException e) {
                 e.printStackTrace();
             }
-        }
-
-        Realm realm = Realm.getDefaultInstance();
-        try {
-            final RealmResults<Geoname> realmResults = realm.where(Geoname.class).findAll();
-            realm.executeTransaction(inRealm -> {
-                cities = realm.copyFromRealm(realmResults);
-                if(cities.size()>0){
-                    try {
-                        PreferencesHelper.savePreference("ChangeCityHourly", true);
-                        PreferencesHelper.savePreference("ChangeCityDaily", true);
-                        PreferencesHelper.savePreference("ChangeCityWeekly", true);
-                    } catch (InvalidClassException e) {
-                        e.printStackTrace();
+        } else {
+            Realm realm = Realm.getDefaultInstance();
+            try {
+                final RealmResults<Geoname> realmResults = realm.where(Geoname.class).findAll();
+                realm.executeTransaction(inRealm -> {
+                    cities = realm.copyFromRealm(realmResults);
+                    if (cities.size() > 0) {
+                        try {
+                            PreferencesHelper.savePreference("ChangeCityHourly", true);
+                            PreferencesHelper.savePreference("ChangeCityDaily", true);
+                            PreferencesHelper.savePreference("ChangeCityWeekly", true);
+                        } catch (InvalidClassException e) {
+                            e.printStackTrace();
+                        }
+                        Intent intent = new Intent(this, ScrollActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Intent intent = new Intent(this, SearchActivity.class);
+                        startActivity(intent);
+                        finish();
                     }
-                    Intent intent = new Intent(this, ScrollActivity.class);
-                    startActivity(intent);
-                }else {
-                    Intent intent = new Intent(this, SearchActivity.class);
-                    startActivity(intent);
-                }
-                Log.d(TAG, "Количество городов: " + cities.size());
-            });
-        }catch (IOException e){
-            Log.d(TAG, e.getMessage());
+                    Log.d(TAG, "Количество городов: " + cities.size());
+                });
+            } catch (IOException e) {
+                Log.d(TAG, e.getMessage());
+            }
         }
     }
 }
